f9881aabcd57dfa40cdc80ce66022040cd672abe
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-04-20 20:05:34 -0300

    Deserializadores de JSON -> Objetos

---
+++ src/main/java/ClienteDeserializer.java
@@ -0,0 +1,37 @@
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+public class ClienteDeserializer implements JsonDeserializer<Cliente> {
+        @Override
+        public Cliente deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+          
+            JsonObject clienteJson = json.getAsJsonObject();
+            Gson gson = new GsonBuilder()
+                            .registerTypeAdapter(Dispositivo.class, new DispositivoDeserializer())
+                            .create();
+            
+          return new Cliente(
+                      clienteJson.get("nombre").getAsString(),
+                      clienteJson.get("apellido").getAsString(),
+                      TipoDeDocumento.valueOf(clienteJson.get("tipoDeDocumento").getAsString()),
+                      clienteJson.get("numeroDeDocumento").getAsInt(),
+                      clienteJson.get("telefono").getAsInt(),
+                      LocalDate.parse(clienteJson.get("fechaDeAlta").getAsString()),
+                      clienteJson.get("domicilio").getAsString(),
+                      clienteJson.get("nombreDeUsuario").getAsString(),
+                      clienteJson.get("contrasena").getAsString(),
+                      Arrays.asList(gson.fromJson(clienteJson.get("dispositivos").getAsString(), Dispositivo[].class))
+                  );
+        }
+
+    }
---
+++ src/main/java/DispositivoDeserializer.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+public class DispositivoDeserializer implements JsonDeserializer<Dispositivo> {
+
+    @Override
+    public Dispositivo deserialize(JsonElement json, Type typeOf, JsonDeserializationContext context)
+            throws JsonParseException {
+        JsonObject dispositivoJson = json.getAsJsonObject();
+        return new Dispositivo(
+                    dispositivoJson.get("nombre").getAsString(),
+                    dispositivoJson.get("kWh").getAsInt(),
+                    dispositivoJson.get("encendido").getAsBoolean()
+                );
+    }
+
+}
