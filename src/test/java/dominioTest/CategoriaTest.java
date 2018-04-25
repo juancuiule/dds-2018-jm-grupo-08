a8e5cb5af14b80144dc4b8fae8db90a541254a9b
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-04-25 20:40:41 -0300

    separa test de categorias y repo

--- src/test/java/dominioTest/CategoriaTest.java
+++ src/test/java/dominioTest/CategoriaTest.java
@@ -2,13 +2,10 @@ package dominioTest;
 
 import static org.junit.Assert.*;
 
-import java.util.Optional;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import dominio.Categoria;
-import dominio.RepositorioCategorias;
 
 public class CategoriaTest {
 
@@ -30,17 +27,4 @@ public class CategoriaTest {
 		assertEquals("Falla - el consumo deberia corresponder a esta categoria",
 				categoriaDePrueba.correspondeCategoria(1800d), false);
 	}
-
-	@Test
-	public void repoDarCategoria_R1_dadoConsumoQueCorresponde() {
-		Optional<Categoria> categoria = RepositorioCategorias.getInstance().categoriaCorrespondiente((double) 120);
-		assertEquals("R1", categoria.get().getCategoria());
-	}
-
-	@Test
-	public void repoDarOtraCategoria_R1_dadoConsumoQueNoCorresponde() {
-		Optional<Categoria> categoria = RepositorioCategorias.getInstance().categoriaCorrespondiente((double) 500);
-		assertNotEquals("El repo da una categoria que no corresponde", "R1", categoria.get().getCategoria());
-	}
-
 }
