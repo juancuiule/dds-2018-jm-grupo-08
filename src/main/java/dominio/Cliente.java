a38cf9041444164f880e858f3275c5d3ade20828
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-04-24 21:25:28 -0300

    metodo recategorizar en Cliente

--- src/main/java/dominio/Cliente.java
+++ src/main/java/dominio/Cliente.java
@@ -1,7 +1,8 @@
 package dominio;
+
 import java.time.LocalDate;
 import java.util.List;
-
+import java.util.Optional;
 
 public class Cliente {
 	private String nombre;
@@ -12,56 +13,61 @@ public class Cliente {
 	private LocalDate fechaDeAlta;
 	private String domicilio;
 	private String nombreDeUsuario;
-	private String contrasena;	
-	private List <Dispositivo> dispositivos;
-	private Categoria categoria;
-	
-    public TipoDeDocumento getTipoDeDocumento() {
-        return tipoDeDocumento;
-    }
-    
-    public LocalDate getFechaDeAlta() {
-        return fechaDeAlta;
-    }
-    public void setCategoria(Categoria unaCategoria) {
-    	this.categoria=unaCategoria;
-    }
+	private String contrasena;
+	private List<Dispositivo> dispositivos;
+	private Optional<Categoria> categoria;
 
-    public Cliente(String nombre, String apellido, TipoDeDocumento tipoDeDocumento, Integer numeroDeDocumento, Integer telefono, LocalDate fechaDeAlta, String domicilio, String nombreDeUsuario, String contrasena, List <Dispositivo> dispositivos) {
+	public TipoDeDocumento getTipoDeDocumento() {
+		return tipoDeDocumento;
+	}
+
+	public LocalDate getFechaDeAlta() {
+		return fechaDeAlta;
+	}
+
+	public void setCategoria(Optional<Categoria> unaCategoria) {
+		this.categoria = unaCategoria;
+	}
+
+	public Cliente(String nombre, String apellido, TipoDeDocumento tipoDeDocumento, Integer numeroDeDocumento,
+			Integer telefono, LocalDate fechaDeAlta, String domicilio, String nombreDeUsuario, String contrasena,
+			List<Dispositivo> dispositivos) {
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.tipoDeDocumento = tipoDeDocumento;
 		this.numeroDeDocumento = numeroDeDocumento;
-		this.telefono = telefono;		
+		this.telefono = telefono;
 		this.fechaDeAlta = fechaDeAlta;
 		this.domicilio = domicilio;
 		this.nombreDeUsuario = nombreDeUsuario;
-		this.contrasena = contrasena;		
-		this.dispositivos = dispositivos; 
+		this.contrasena = contrasena;
+		this.dispositivos = dispositivos;
 	}
-	
+
 	public Boolean hayAlgunDispositivoEncendido() {
 		return this.dispositivos.stream().anyMatch((Dispositivo dispositivo) -> dispositivo.estaEncendido());
 	}
-	
+
 	public Long cantidadDeDispositivosEncendido() {
 		return this.dispositivos.stream().filter((Dispositivo dispositivo) -> dispositivo.estaEncendido()).count();
 	}
-	
+
 	public Long cantidadDeDispositivosApagados() {
 		return this.dispositivos.stream().filter((Dispositivo dispositivo) -> dispositivo.estaApagado()).count();
 	}
-	 
+
 	public Integer cantidadDeDispositivos() {
 		return this.dispositivos.size();
 	}
+
 	public Double consumo() {
-		return this.dispositivos.stream()
-				.filter((Dispositivo dispositivo) -> dispositivo.estaEncendido())
+		return this.dispositivos.stream().filter((Dispositivo dispositivo) -> dispositivo.estaEncendido())
 				.mapToDouble((Dispositivo dispositivo) -> dispositivo.getkWh()).sum();
 	}
-	/*public void recategorizar() {
-		RepositorioCategorias repositorio=RepositorioCategorias.getInstance();
-	}*/
-	
+
+	public void recategorizar() {
+		RepositorioCategorias repositorio = RepositorioCategorias.getInstance();
+		this.categoria = repositorio.categoriaCorrespondiente(this.consumo());
+	}
+
 }
