a9179d1e61954896ccc743ffc861629ce8bcc9e0
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-04-24 21:24:57 -0300

    cambia parametro de categoriaCorrespondiente y correspondeCategoria, de Cliente a consumo

--- src/main/java/dominio/Categoria.java
+++ src/main/java/dominio/Categoria.java
@@ -1,30 +1,34 @@
 package dominio;
 
 public class Categoria {
-    private String categoria;
-    private Integer limiteInferiorDeConsumo;
-    private Integer limiteSuperiorDeConsumo;
-    private Double valorFijo;
-    private Double valorVariable;
+	private String categoria;
+	private Integer limiteInferiorDeConsumo;
+	private Integer limiteSuperiorDeConsumo;
+	private Double valorFijo;
+	private Double valorVariable;
 
-    public Categoria(String categoria, Integer limiteInferiorDeConsumo, Integer limiteSuperiorDeConsumo, Double valorFijo, Double valorVariable) {
-        this.categoria = categoria;
-        this.limiteInferiorDeConsumo = limiteInferiorDeConsumo;
-        this.limiteSuperiorDeConsumo = limiteSuperiorDeConsumo;
-        this.valorFijo = valorFijo ;
-        this.valorVariable  = valorVariable ;
-    }
+	public Categoria(String categoria, Integer limiteInferiorDeConsumo, Integer limiteSuperiorDeConsumo,
+			Double valorFijo, Double valorVariable) {
+		this.categoria = categoria;
+		this.limiteInferiorDeConsumo = limiteInferiorDeConsumo;
+		this.limiteSuperiorDeConsumo = limiteSuperiorDeConsumo;
+		this.valorFijo = valorFijo;
+		this.valorVariable = valorVariable;
+	}
 
-    double cargoFijo(Integer meses) {
-        return this.valorFijo * meses;
-    }
+	public String getCategoria() {
+		return this.categoria;
+	}
 
-    double cargoVariable(Double kWh) {
-        return this.valorVariable * kWh;
-    }
+	public double cargoFijo(Integer meses) {
+		return this.valorFijo * meses;
+	}
 
-    public boolean correspondeCategoria(Cliente cliente) {
-        return (this.limiteInferiorDeConsumo < cliente.consumo()) &&
-                (cliente.consumo() <= this.limiteSuperiorDeConsumo);
-    }
+	public double cargoVariable(Double kWh) {
+		return this.valorVariable * kWh;
+	}
+
+	public boolean correspondeCategoria(Double consumo) {
+		return (this.limiteInferiorDeConsumo < consumo) && (consumo <= this.limiteSuperiorDeConsumo);
+	}
 }
--- src/main/java/dominio/RepositorioCategorias.java
+++ src/main/java/dominio/RepositorioCategorias.java
@@ -6,36 +6,27 @@ import java.util.List;
 import java.util.Optional;
 
 public class RepositorioCategorias {
-    static RepositorioCategorias instancia;
-    private List<Categoria> categorias = new ArrayList<Categoria>(
-        Arrays.asList(
-            new Categoria("R1", 0, 150, 18.76, 0.644),
-            new Categoria("R2", 150, 325, 35.32, 0.644),
-            new Categoria("R3", 325, 400, 60.71, 0.681),
-            new Categoria("R4", 400, 450, 71.74, 0.738),
-            new Categoria("R5", 450, 500, 110.38, 0.794),
-            new Categoria("R6", 500, 600, 220.75, 0.832),
-            new Categoria("R7", 600, 700, 443.59, 0.851),
-            new Categoria("R8", 700, 1400, 545.96, 0.851),
-            new Categoria("R9", 1400, Integer.MAX_VALUE, 887.19, 0.851)
-        )
-    );
-    
-    public static RepositorioCategorias getInstance() {
-        if(instancia == null) {
-            return new RepositorioCategorias();
-        }
-        return instancia;
-    }
-    
-    public List<Categoria> categorias() {
-        return this.categorias;
-    }
-    
-    public Optional<Categoria> categoriaCorrespondiente(Cliente cliente){
-        return this.categorias.stream()
-                              .filter(categoria -> categoria.correspondeCategoria(cliente))
-                              .findFirst();
-    }
-    
+	static RepositorioCategorias instancia;
+	private List<Categoria> categorias = new ArrayList<Categoria>(
+			Arrays.asList(new Categoria("R1", 0, 150, 18.76, 0.644), new Categoria("R2", 150, 325, 35.32, 0.644),
+					new Categoria("R3", 325, 400, 60.71, 0.681), new Categoria("R4", 400, 450, 71.74, 0.738),
+					new Categoria("R5", 450, 500, 110.38, 0.794), new Categoria("R6", 500, 600, 220.75, 0.832),
+					new Categoria("R7", 600, 700, 443.59, 0.851), new Categoria("R8", 700, 1400, 545.96, 0.851),
+					new Categoria("R9", 1400, Integer.MAX_VALUE, 887.19, 0.851)));
+
+	public static RepositorioCategorias getInstance() {
+		if (instancia == null) {
+			return new RepositorioCategorias();
+		}
+		return instancia;
+	}
+
+	public List<Categoria> categorias() {
+		return this.categorias;
+	}
+
+	public Optional<Categoria> categoriaCorrespondiente(Double consumo) {
+		return this.categorias.stream().filter(categoria -> categoria.correspondeCategoria(consumo)).findFirst();
+	}
+
 }
