2ffb87090d137c0854756d8c4428df71bc095559
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-05-01 23:17:03 -0300

    fix filtrarSegun en repos

--- src/main/java/dominio/Repositorio.java
+++ src/main/java/dominio/Repositorio.java
@@ -5,12 +5,13 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public abstract class Repositorio<T> {
-	private List<T> elementos;
-	public List<T> elementos(){
+	public List<T> elementos;
+
+	public List<T> elementos() {
 		return this.elementos;
 	}
-	public Stream<T> filtrarSegun(Predicate<T> unaCondicion){
+
+	public Stream<T> filtrarSegun(Predicate<T> unaCondicion) {
 		return this.elementos().stream().filter(unaCondicion);
 	}
-
 }
