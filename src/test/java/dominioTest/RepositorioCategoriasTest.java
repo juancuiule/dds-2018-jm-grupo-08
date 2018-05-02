5e0ce6580c8cbdf2d13c47019453a1bf4d968aea
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-05-01 23:23:07 -0300

    fix tests de repoCategorias

--- src/test/java/dominioTest/RepositorioCategoriasTest.java
+++ src/test/java/dominioTest/RepositorioCategoriasTest.java
@@ -12,13 +12,13 @@ import dominio.RepositorioCategorias;
 public class RepositorioCategoriasTest {
 	@Test
 	public void repoDarCategoria_R1_dadoConsumoQueCorresponde() {
-		Optional<Categoria> categoria = RepositorioCategorias.getInstance().categoriaCorrespondiente((double) 120);
-		assertEquals("R1", categoria.get().getCategoria());
+		Categoria categoria = RepositorioCategorias.getInstance().categoriaCorrespondiente((double) 120);
+		assertEquals("R1", categoria.getCategoria());
 	}
 
 	@Test
 	public void repoDarOtraCategoria_R1_dadoConsumoQueNoCorresponde() {
-		Optional<Categoria> categoria = RepositorioCategorias.getInstance().categoriaCorrespondiente((double) 500);
-		assertNotEquals("R1", categoria.get().getCategoria());
+		Categoria categoria = RepositorioCategorias.getInstance().categoriaCorrespondiente((double) 500);
+		assertNotEquals("R1", categoria.getCategoria());
 	}
 }
