aaf064cd4327f52226f606f106aa3fd510ea8fc5
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-06-20 20:08:19 -0300

    Cambio de implementacion de dispositivos

--- src/main/java/dominio/dispositivo/Dispositivo.java
+++ src/main/java/dominio/dispositivo/Dispositivo.java
@@ -1,5 +1,48 @@
 package dominio.dispositivo;
 
+import java.time.Period;
+
 public class Dispositivo {
-    private Comportamiento comportamineto;
+    private Comportamiento comportamiento;
+    public Boolean estaEncendido() {
+        try{
+            return comportamiento.estaEncendido();
+        }catch(ComportamientoNoAdmitidoException  e){
+            return false;
+        }
+    }
+    public Boolean estaApagado(){
+        try{
+            return comportamiento.estaApagado();
+        }catch(ComportamientoNoAdmitidoException  e){
+            return false;
+        }
+    }
+    public Double consumoEnUltimasHoras(Double horas){
+        return comportamiento.consumoEnUltimasHoras(horas);
+    }
+    public Double consumoEnElPeriodo(Period periodo){
+        return comportamiento.consumoEnElPeriodo(periodo);
+    }
+    public void apagar(){
+        try{
+            comportamiento.apagar();
+        }catch(ComportamientoNoAdmitidoException e){
+            // Nada
+        }
+    }
+    public void encender(){
+        try{
+            comportamiento.encender();
+        }catch(ComportamientoNoAdmitidoException e){
+            // Nada
+        }
+    }
+    public void ahorrarEnergia(){
+        try{
+            comportamiento.ahorrarEnergia();
+        }catch(ComportamientoNoAdmitidoException e){
+            // Nada
+        }
+    }
 }
