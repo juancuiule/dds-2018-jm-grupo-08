ced39702acbac3ab2366982644b67f85f0f5b06a
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-06-21 00:22:07 -0300

    Comentados tests obsoletos

--- src/test/java/dominioTest/ClienteTest.java
+++ src/test/java/dominioTest/ClienteTest.java
@@ -3,6 +3,7 @@ package dominioTest;
 import static org.junit.Assert.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -13,11 +14,6 @@ import dominio.Cliente;
 import dominio.dispositivo.Dispositivo;
 import dominio.dispositivo.ComportamientoEstandar;
 import dominio.TipoDeDocumento;
-
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 
 
@@ -39,30 +35,30 @@ public class ClienteTest {
 				"8944 Red Saturn Dr", "arthhow", "hotshot", new ArrayList<Dispositivo>());
 	}
 
-	@Test
-	public void elClienteATieneUnDispositivoEncendido() {
-		assertTrue(clienteA.hayAlgunDispositivoEncendido());
-	}
+//	@Test
+//	public void elClienteATieneUnDispositivoEncendido() {
+//		assertTrue(clienteA.hayAlgunDispositivoEncendido());
+//	}
 
-	@Test
-	public void elClienteATieneDosDispositivosEncendidos() {
-		assertEquals(2, clienteA.cantidadDeDispositivosEncendidos());
-	}
+//	@Test
+//	public void elClienteATieneDosDispositivosEncendidos() {
+//		assertEquals(2, clienteA.cantidadDeDispositivosEncendidos());
+//	}
 
-	@Test
-	public void elClienteATieneUnDispositivoApagado() {
-		assertEquals(1, clienteA.cantidadDeDispositivosApagados());
-	}
+//	@Test
+//	public void elClienteATieneUnDispositivoApagado() {
+//		assertEquals(1, clienteA.cantidadDeDispositivosApagados());
+//	}
 
 	@Test
 	public void elClienteATieneTresDispositivos() {
 		assertEquals(3, clienteA.cantidadDeDispositivos());
 	}
 
-	@Test
-	public void consumoDelClienteA() {
-		assertEquals(new Double(1.75), clienteA.consumo());
-	}
+//	@Test
+//	public void consumoDelClienteA() {
+//		assertEquals(new Double(1.75), clienteA.consumo(periodoUltimoMes()));
+//	}
 
 	@Test
 	public void elClienteBNoTieneUnDispositivoEncendido() {
@@ -71,26 +67,30 @@ public class ClienteTest {
 
 	@Test
 	public void elConsumoDelClienteB() {
-		assertEquals(new Double(0), clienteB.consumo());
+		assertEquals(new Double(0), clienteB.consumo(periodoUltimoMes()));
 	}
 
-	@Test
-	public void elClienteACambiaDeCategoria() {
-		Dispositivo lavaplatos = new Dispositivo("Heladera", new ComportamientoEstandar(320.023, 12.0));
-		Dispositivo plancha = new Dispositivo("Tostadora", new ComportamientoEstandar(120.123, 12.0));
-		clienteA.agregarDispositivo(lavaplatos);
-		clienteA.agregarDispositivo(plancha);
-		clienteA.recategorizar();
-		assertEquals("R4", clienteA.categoria().getNombre());
-	}
+//	@Test
+//	public void elClienteACambiaDeCategoria() {
+//		Dispositivo lavaplatos = new Dispositivo("Heladera", new ComportamientoEstandar(320.023, 12.0));
+//		Dispositivo plancha = new Dispositivo("Tostadora", new ComportamientoEstandar(120.123, 12.0));
+//		clienteA.agregarDispositivo(lavaplatos);
+//		clienteA.agregarDispositivo(plancha);
+//		clienteA.recategorizar();
+//		assertEquals("R4", clienteA.categoria().getNombre());
+//	}
 
-	@Test
-	public void elClienteBCambiaDeCategoria() {
-		Dispositivo heladera = new Dispositivo("Heladera con Freezer",new ComportamientoEstandar(0.4, 12.0));
-		Dispositivo tostadora = new Dispositivo("Tostadora",new ComportamientoEstandar(1.0, 12.0));
-		clienteB.agregarDispositivo(heladera);
-		clienteB.agregarDispositivo(tostadora);
-		clienteB.recategorizar();
-		assertEquals("R1", clienteB.categoria().getNombre());
+//	@Test
+//	public void elClienteBCambiaDeCategoria() {
+//		Dispositivo heladera = new Dispositivo("Heladera con Freezer",new ComportamientoEstandar(0.4, 12.0));
+//		Dispositivo tostadora = new Dispositivo("Tostadora",new ComportamientoEstandar(1.0, 12.0));
+//		clienteB.agregarDispositivo(heladera);
+//		clienteB.agregarDispositivo(tostadora);
+//		clienteB.recategorizar();
+//		assertEquals("R1", clienteB.categoria().getNombre());
+//	}
+	
+	private Period periodoUltimoMes() {
+		return Period.between(LocalDate.now().plusMonths(-1), LocalDate.now());
 	}
 }
