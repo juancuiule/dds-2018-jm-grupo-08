00fe336fa04a05777c876adc4e39bd522abf059a
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-06-21 00:29:27 -0300

    Fix repositorios

--- src/main/java/dominio/RepositorioCategorias.java
+++ src/main/java/dominio/RepositorioCategorias.java
@@ -2,7 +2,6 @@ package dominio;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class RepositorioCategorias extends Repositorio<Categoria> {
 	static RepositorioCategorias instancia;
