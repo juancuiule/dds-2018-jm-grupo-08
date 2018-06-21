f8a35fa0d5f5c91ea3620cfce869dd9a04841399
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-06-20 23:42:35 -0300

    agrego Regla

---
+++ src/main/java/dominio/Regla.java
@@ -0,0 +1,45 @@
+package dominio;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import dominio.dispositivo.Dispositivo;
+
+public class Regla {
+
+	List<Actuador> actuadores;
+	Dispositivo dispositivo;
+	// Sensor sensor;
+	// Predicate<Sensor> condicion;
+	
+	public Regla(
+			List<Actuador> unosActuadores,
+			Dispositivo unDispositivo
+			/*, 
+			Sensor unSensor,
+			Predicate<Sensor> unaCondicion
+			*/
+	) {
+		// sensor = unSensor;
+		// condicion = unaCondicion;
+		actuadores = unosActuadores;
+		dispositivo = unDispositivo;
+	}
+	
+//	public ejecutarSiCorresponde() {
+//		if(this.comprobarCondicion()) {
+//			this.ejecutar();
+//		}
+//	}
+	
+//	public boolean comprobarCondicion() {
+//		return condicion.test(sensor)
+//	}
+	
+	private void ejecutar() {
+		actuadores.forEach(actuador ->
+			actuador.actuar(this.dispositivo)
+		);
+	}	
+	
+}
