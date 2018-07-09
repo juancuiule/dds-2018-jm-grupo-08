52fbc586635891b441e3988f470e220a3fce7b80
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-07-08 23:04:28 -0300

    borro comentarios de eclipse

--- src/test/java/dominioTest/mocks/InterfazDeFabricaMock.java
+++ src/test/java/dominioTest/mocks/InterfazDeFabricaMock.java
@@ -7,51 +7,43 @@ import dominio.dispositivo.InterfazDeFabrica;
 public class InterfazDeFabricaMock implements InterfazDeFabrica {
 
 	Boolean encendido = false;
-	
+
 	@Override
 	public Double consumoPorHora() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Double consumoEnLasUltimasHoras() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Double consumoEnElPeriodo(Period periodo) {
-		// TODO Auto-generated method stub
-		return null;
+		return (double) 100;
 	}
 
 	@Override
 	public void ahorrarEnergia() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public Boolean estaEncendido() {
-		// TODO Auto-generated method stub
 		return encendido;
 	}
 
 	@Override
 	public Boolean estaApagado() {
-		// TODO Auto-generated method stub
 		return !encendido;
 	}
 
 	@Override
 	public void apagar() {
-		// TODO Auto-generated method stub
 		encendido = false;
 	}
 
 	@Override
 	public void encender() {
-		// TODO Auto-generated method stub
 		encendido = true;
 	}
 }
