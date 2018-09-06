cab3d3fac825a6537195c029a9673eb9a87723d5
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-09-06 08:27:00 -0300

    formato de código

--- src/main/java/dominio/dispositivo/ComportamientoInteligente.java
+++ src/main/java/dominio/dispositivo/ComportamientoInteligente.java
@@ -3,61 +3,61 @@ package dominio.dispositivo;
 import java.time.Period;
 
 public class ComportamientoInteligente implements Comportamiento {
-    private InterfazDeFabrica interfazDeFabrica;
-    private Double consumoPorHora;
+	private InterfazDeFabrica interfazDeFabrica;
+	private Double consumoPorHora;
 
-    public ComportamientoInteligente(InterfazDeFabrica interfazDeFabrica) {
-        this.interfazDeFabrica = interfazDeFabrica;
-    }
-    
-    public ComportamientoInteligente(InterfazDeFabrica interfazDeFabrica, Double consumoPorHora) {
-        super();
-        this.interfazDeFabrica = interfazDeFabrica;
-        this.consumoPorHora = consumoPorHora;
-    }
+	public ComportamientoInteligente(InterfazDeFabrica interfazDeFabrica) {
+		this.interfazDeFabrica = interfazDeFabrica;
+	}
 
-    public Double consumoPorHora() {
-    	return consumoPorHora;
-    }
+	public ComportamientoInteligente(InterfazDeFabrica interfazDeFabrica, Double consumoPorHora) {
+		super();
+		this.interfazDeFabrica = interfazDeFabrica;
+		this.consumoPorHora = consumoPorHora;
+	}
 
-    @Override
-    public Boolean estaEncendido() {
-        return interfazDeFabrica.estaEncendido();
-    }
+	public Double consumoPorHora() {
+		return consumoPorHora;
+	}
 
-    @Override
-    public Boolean estaApagado() {
-        return interfazDeFabrica.estaApagado();
-    }
+	@Override
+	public Boolean estaEncendido() {
+		return interfazDeFabrica.estaEncendido();
+	}
 
-    @Override
-    public Double consumoEnUltimasHoras(Double horas) {
-        return interfazDeFabrica.consumoEnLasUltimasHoras();
-    }
+	@Override
+	public Boolean estaApagado() {
+		return interfazDeFabrica.estaApagado();
+	}
 
-    @Override
-    public Double consumoEnElPeriodo(Period periodo) {
-        return interfazDeFabrica.consumoEnElPeriodo(periodo);
-    }
+	@Override
+	public Double consumoEnUltimasHoras(Double horas) {
+		return interfazDeFabrica.consumoEnLasUltimasHoras();
+	}
 
-    @Override
-    public void apagar() {
-        interfazDeFabrica.apagar();
-    }
+	@Override
+	public Double consumoEnElPeriodo(Period periodo) {
+		return interfazDeFabrica.consumoEnElPeriodo(periodo);
+	}
 
-    @Override
-    public void encender() {
-        interfazDeFabrica.encender();
-    }
+	@Override
+	public void apagar() {
+		interfazDeFabrica.apagar();
+	}
 
-    @Override
-    public void ahorrarEnergia() {
-        interfazDeFabrica.ahorrarEnergia();
-    }
+	@Override
+	public void encender() {
+		interfazDeFabrica.encender();
+	}
+
+	@Override
+	public void ahorrarEnergia() {
+		interfazDeFabrica.ahorrarEnergia();
+	}
 
 	@Override
 	public void restringirConsumo() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
