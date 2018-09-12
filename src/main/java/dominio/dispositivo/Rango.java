63087d7773622433c2b88202def58a9eca357b5e
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-09-12 19:34:17 -0300

    Fix test uno

--- src/main/java/dominio/dispositivo/Rango.java
+++ src/main/java/dominio/dispositivo/Rango.java
@@ -6,21 +6,34 @@ import javax.persistence.*;
 
 @Embeddable
 public class Rango {
-
+    // Atributes
     private Double cotaSuperior;
     private Double cotaInferior;
+
+    // Constructors
+    public Rango() {
+    }
+
     public Rango(Double cotaSuperior, Double cotaInferior) {
         super();
         this.cotaSuperior = cotaSuperior;
         this.cotaInferior = cotaInferior;
     }
+
+    // Accessors
     public Double getCotaSuperior() {
         return cotaSuperior;
     }
-    
+
+    public void setCotaSuperior(Double cotaSuperior) {
+        this.cotaSuperior = cotaSuperior;
+    }
+
     public Double getCotaInferior() {
         return cotaInferior;
     }
-    
 
+    public void setCotaInferior(Double cotaInferior) {
+        this.cotaInferior = cotaInferior;
+    }
 }
--- src/test/java/db/PruebaUnoTest.java
+++ src/test/java/db/PruebaUnoTest.java
@@ -1,83 +1,61 @@
 package db;
 
+import dominio.Cliente;
+import dominio.TipoDeDocumento;
+import dominio.dispositivo.ComportamientoEstandar;
+import dominio.dispositivo.ComportamientoInteligente;
+import dominio.dispositivo.Dispositivo;
+import dominio.dispositivo.Rango;
+import dominio.transformadores.Punto;
+import dominio.transformadores.RepositorioTransformadores;
+import dominio.transformadores.Transformador;
+import dominioTest.mocks.InterfazDeFabricaMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
+
+import javax.persistence.Query;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Query;
+public class PruebaUnoTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
+    Cliente raul;
 
-import dominio.Cliente;
-import dominio.TipoDeDocumento;
-import dominio.dispositivo.Comportamiento;
-import dominio.dispositivo.ComportamientoEstandar;
-import dominio.dispositivo.ComportamientoInteligente;
-import dominio.dispositivo.Dispositivo;
-import dominio.dispositivo.DispositivoFactory;
-import dominio.dispositivo.Rango;
-import dominio.transformadores.Punto;
-import dominioTest.mocks.InterfazDeFabricaMock;
+    @Before
+    public void fixture() {
+        RepositorioTransformadores.getInstance().agregar(new Transformador(new Punto(2d,2d),true));
+        raul = new Cliente("Raul","Gomez",TipoDeDocumento.DNI,
+                new Integer(33333334), new Integer(800445),
+                LocalDate.of(2018, 01, 11), "Lujan","raulG","1234",
+                this.listaDeDispositivos(),true,new Punto(1d,2d));
+    }
 
-public class PruebaUnoTest {
+    @Test
+    public void persistirAraul() {
+        entityManager().persist(raul);
+        Query query= entityManager().createQuery("from Cliente");
+        List<Cliente> clienteRaul = query.getResultList();
+        Assert.assertEquals(clienteRaul.get(0), raul);
+    }
 
-	EntityManager manager = PerThreadEntityManagers.getEntityManager();
-	
-	EntityTransaction transaction =  manager.getTransaction();
-	
-	Cliente raul = new Cliente("Raul","Gomez",TipoDeDocumento.DNI,
-			new Integer(33333334), new Integer(800445), 
-			LocalDate.of(2018, 01, 11), "Lujan","raulG","1234",
-			this.listaDeDispositivos(),true,new Punto(1d,2d));
-	
-	@Before
-	public void transaccionBegin() {
-		transaction.begin();
-	}
-	
-	@After
-	public void transaccionRollback() {
-		transaction.rollback();
-	}
-	
-	
-	@Test
-	public void persistirAraul() {
-		 manager.persist(raul);
-		 
-		 Query query= manager.createQuery("from Cliente");
-		 
-		 List<Cliente> clienteRaul = query.getResultList();
-		 
-		 Assert.assertEquals(clienteRaul.get(0), raul);
-	}
-	
-	
-	
-	
-	private List<Dispositivo> listaDeDispositivos(){
-		
-		
-		ComportamientoInteligente comportamientoI = new ComportamientoInteligente(new InterfazDeFabricaMock(),0.2);
-		ComportamientoEstandar conportamientoE = new ComportamientoEstandar(1.2, 6d);
-		List<Dispositivo> dispositivos = new ArrayList<Dispositivo>();
-		Dispositivo aire3500 = new Dispositivo(comportamientoI, "aire", new Rango(0.1, 0.72));
-		Dispositivo ventiladorPie = new Dispositivo(conportamientoE, "ventilador", new Rango(0.24, 0.745));
-		
-		
-		dispositivos.add(ventiladorPie);
-		dispositivos.add(aire3500);	
-		
-		return dispositivos;
-		
-	}
-	
-	
-	
+    // Private methods
+    private List<Dispositivo> listaDeDispositivos(){
+        ComportamientoInteligente comportamientoI = new ComportamientoInteligente(new InterfazDeFabricaMock(),0.2);
+        ComportamientoEstandar conportamientoE = new ComportamientoEstandar(1.2, 6d);
+        List<Dispositivo> dispositivos = new ArrayList<Dispositivo>();
+        Dispositivo aire3500 = new Dispositivo(comportamientoI, "aire", new Rango(0.1, 0.72));
+        Dispositivo ventiladorPie = new Dispositivo(conportamientoE, "ventilador", new Rango(0.24, 0.745));
+
+        dispositivos.add(ventiladorPie);
+        dispositivos.add(aire3500);
+
+        return dispositivos;
+    }
+
 }
+
+
