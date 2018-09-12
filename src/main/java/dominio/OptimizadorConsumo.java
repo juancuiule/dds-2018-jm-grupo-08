28d35aaf8155f40de87d9f272fe6338a8ae0c9e2
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-09-12 17:07:24 -0300

    format code

--- src/main/java/dominio/OptimizadorConsumo.java
+++ src/main/java/dominio/OptimizadorConsumo.java
@@ -20,103 +20,102 @@ import dominio.dispositivo.Dispositivo;
 import dominio.dispositivo.NoExistenRestriccionesException;
 
 public class OptimizadorConsumo {
-    
-    public static List<Optimizacion> optimizar(List<Dispositivo> dispositivos) {
-    	// Limite mensua de consumo desde configuracion
-    	Double limiteMensual = ConfiguracionApp.limiteMensualDeConsumo;
-    	
-        // Filtrar dispositivos sin restricciones
-        List<Dispositivo> dispositivosFiltrados = dispositivos
-                                                 .stream()
-                                                 .filter(dispositivo -> {
-                                                     return dispositivo.tieneRestricciones();
-                                                 }).collect(Collectors.toList());
-        
-        // Funcion economica-objetivo
-        double[] arrayObjetivo = new double[dispositivosFiltrados.size()];
-        Arrays.fill(arrayObjetivo, 1);
-        LinearObjectiveFunction funcionZ = new LinearObjectiveFunction(arrayObjetivo, 0);
-        
-        
-        // RESTRICCIONES
-        List<LinearConstraint> restricciones = new ArrayList<LinearConstraint>();
-        
-        // Restriccion mensual
-        double[] coeficientesRestriccion = arrayDeCoeficientes(dispositivosFiltrados);
-        LinearConstraint restriccionMensual = new LinearConstraint(coeficientesRestriccion, Relationship.LEQ, limiteMensual);
-        restricciones.add(restriccionMensual);
-        
-        // Restricciones por dispositivo
-        dispositivosFiltrados.forEach(dispositivo -> {
-            LinearConstraint restriccionSuperior = generarRestricciones(dispositivosFiltrados,dispositivo,Relationship.LEQ);
-            LinearConstraint restriccionInferior = generarRestricciones(dispositivosFiltrados,dispositivo,Relationship.GEQ);
-            restricciones.add(restriccionSuperior);
-            restricciones.add(restriccionInferior);
-        });
-        
-        // Solucionar problema
-        SimplexSolver solver = new SimplexSolver();
-        PointValuePair solucion = solver.optimize(new MaxIter(100)                        // Numero maximo de iteraciones
-                                                , funcionZ                                // Funcion objetivo
-                                                , new LinearConstraintSet(restricciones)  // Restricciones
-                                                , GoalType.MAXIMIZE                       // Objetivo (Maximizar)
-                                                , new NonNegativeConstraint(true));       // Restriccion adicional ( solo positivos )
-        
-        // Genear solucion
-        List<Double> limitesDeRestriccion = listFromDoubleArray(solucion.getPoint());
-        return generarOptimizaciones(dispositivosFiltrados,limitesDeRestriccion);
-    }
-    
-    /////////////////
-    /////////////////
-    /////////////////
-    
-    private static List<Double> listFromDoubleArray(double[] array) {
-        List<Double> resultado = new ArrayList<Double>();
-        for(int i = 0; i < array.length; i++) {
-            resultado.add(new Double(array[i]));
-        }
-        return resultado;
-    }
 
-    private static LinearConstraint generarRestricciones(List<Dispositivo> dispositivos, Dispositivo dispositivo, Relationship relacion) {
-        Integer posicionEnLista = dispositivos.indexOf(dispositivo);
-        double[] arrayRestriccion = new double[dispositivos.size()];
-        Arrays.fill(arrayRestriccion, 0d);
-        arrayRestriccion[posicionEnLista] = 1;
-        double limite;
-        if(relacion == Relationship.LEQ) {
-            limite = dispositivos.get(posicionEnLista).restricciones().getCotaSuperior().doubleValue();
-        }else if(relacion == Relationship.GEQ){
-            limite = dispositivos.get(posicionEnLista).restricciones().getCotaInferior().doubleValue();
-        }else {
-            throw new RuntimeException ("Relacion no admitida");
-        }
+	public static List<Optimizacion> optimizar(List<Dispositivo> dispositivos) {
+		// Limite mensua de consumo desde configuracion
+		Double limiteMensual = ConfiguracionApp.limiteMensualDeConsumo;
 
-        LinearConstraint restriccion = new LinearConstraint(arrayRestriccion, relacion, limite);
-        return restriccion;
-    }
+		// Filtrar dispositivos sin restricciones
+		List<Dispositivo> dispositivosFiltrados = dispositivos.stream().filter(dispositivo -> {
+			return dispositivo.tieneRestricciones();
+		}).collect(Collectors.toList());
 
-    private static double[] arrayDeCoeficientes(List<Dispositivo> dispositivos) {
-        Double[] coeficientes = new Double[dispositivos.size()];
-        coeficientes = dispositivos.stream()
-                                   .map(dispositivo -> dispositivo.consumoPorHora())
-                                   .collect(Collectors.toList())
-                                   .toArray(coeficientes);
-        double[] coeficientesPrimitivos = ArrayUtils.toPrimitive(coeficientes);
-        return coeficientesPrimitivos;
-    }
-    
-    private static List<Optimizacion> generarOptimizaciones(List<Dispositivo> dispositivos, List<Double> limites){
-    	List<Optimizacion> restricciones= new ArrayList<Optimizacion>();
-    	
-		for(int i = 0; i< dispositivos.size(); i++){
+		// Funcion economica-objetivo
+		double[] arrayObjetivo = new double[dispositivosFiltrados.size()];
+		Arrays.fill(arrayObjetivo, 1);
+		LinearObjectiveFunction funcionZ = new LinearObjectiveFunction(arrayObjetivo, 0);
+
+		// RESTRICCIONES
+		List<LinearConstraint> restricciones = new ArrayList<LinearConstraint>();
+
+		// Restriccion mensual
+		double[] coeficientesRestriccion = arrayDeCoeficientes(dispositivosFiltrados);
+		LinearConstraint restriccionMensual = new LinearConstraint(coeficientesRestriccion, Relationship.LEQ,
+				limiteMensual);
+		restricciones.add(restriccionMensual);
+
+		// Restricciones por dispositivo
+		dispositivosFiltrados.forEach(dispositivo -> {
+			LinearConstraint restriccionSuperior = generarRestricciones(dispositivosFiltrados, dispositivo,
+					Relationship.LEQ);
+			LinearConstraint restriccionInferior = generarRestricciones(dispositivosFiltrados, dispositivo,
+					Relationship.GEQ);
+			restricciones.add(restriccionSuperior);
+			restricciones.add(restriccionInferior);
+		});
+
+		// Solucionar problema
+		SimplexSolver solver = new SimplexSolver();
+		PointValuePair solucion = solver.optimize(new MaxIter(100) // Numero maximo de iteraciones
+				, funcionZ // Funcion objetivo
+				, new LinearConstraintSet(restricciones) // Restricciones
+				, GoalType.MAXIMIZE // Objetivo (Maximizar)
+				, new NonNegativeConstraint(true)); // Restriccion adicional ( solo positivos )
+
+		// Genear solucion
+		List<Double> limitesDeRestriccion = listFromDoubleArray(solucion.getPoint());
+		return generarOptimizaciones(dispositivosFiltrados, limitesDeRestriccion);
+	}
+
+	/////////////////
+	/////////////////
+	/////////////////
+
+	private static List<Double> listFromDoubleArray(double[] array) {
+		List<Double> resultado = new ArrayList<Double>();
+		for (int i = 0; i < array.length; i++) {
+			resultado.add(new Double(array[i]));
+		}
+		return resultado;
+	}
+
+	private static LinearConstraint generarRestricciones(List<Dispositivo> dispositivos, Dispositivo dispositivo,
+			Relationship relacion) {
+		Integer posicionEnLista = dispositivos.indexOf(dispositivo);
+		double[] arrayRestriccion = new double[dispositivos.size()];
+		Arrays.fill(arrayRestriccion, 0d);
+		arrayRestriccion[posicionEnLista] = 1;
+		double limite;
+		if (relacion == Relationship.LEQ) {
+			limite = dispositivos.get(posicionEnLista).restricciones().getCotaSuperior().doubleValue();
+		} else if (relacion == Relationship.GEQ) {
+			limite = dispositivos.get(posicionEnLista).restricciones().getCotaInferior().doubleValue();
+		} else {
+			throw new RuntimeException("Relacion no admitida");
+		}
+
+		LinearConstraint restriccion = new LinearConstraint(arrayRestriccion, relacion, limite);
+		return restriccion;
+	}
+
+	private static double[] arrayDeCoeficientes(List<Dispositivo> dispositivos) {
+		Double[] coeficientes = new Double[dispositivos.size()];
+		coeficientes = dispositivos.stream().map(dispositivo -> dispositivo.consumoPorHora())
+				.collect(Collectors.toList()).toArray(coeficientes);
+		double[] coeficientesPrimitivos = ArrayUtils.toPrimitive(coeficientes);
+		return coeficientesPrimitivos;
+	}
+
+	private static List<Optimizacion> generarOptimizaciones(List<Dispositivo> dispositivos, List<Double> limites) {
+		List<Optimizacion> restricciones = new ArrayList<Optimizacion>();
+
+		for (int i = 0; i < dispositivos.size(); i++) {
 			Dispositivo dispositivo = dispositivos.get(i);
 			Double limite = limites.get(i);
-			
+
 			restricciones.add(new Optimizacion(dispositivo, limite));
 		}
-    	return restricciones;
-    }
-    
+		return restricciones;
+	}
+
 }
