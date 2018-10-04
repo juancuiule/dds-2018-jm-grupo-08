bc83e41888bd617df395f56abf37c18450ce1a9b
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-10-04 19:19:38 -0300

    God method fragmentado

--- src/main/java/dominio/OptimizadorConsumo.java
+++ src/main/java/dominio/OptimizadorConsumo.java
@@ -1,12 +1,9 @@
 package dominio;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
-import java.util.stream.Stream;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.math3.optim.MaxIter;
@@ -20,95 +17,108 @@ import org.apache.commons.math3.optim.linear.SimplexSolver;
 import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
 
 import dominio.dispositivo.Dispositivo;
-import dominio.dispositivo.NoExistenRestriccionesException;
 import utils.Utils;
 
 public class OptimizadorConsumo {
 
-	public static List<Optimizacion> optimizar(List<Dispositivo> dispositivos) {
-		// Limite mensua de consumo desde configuracion
-		Double limiteMensual = ConfiguracionApp.limiteMensualDeConsumo;
+    public static List<Optimizacion> optimizar(List<Dispositivo> dispositivos) {
 
-		// filter: me quedo con dispositivos que tienen restricciones
-		List<Dispositivo> dispositivosConRestricciones = dispositivos.stream().filter(dispositivo -> {
-			return dispositivo.tieneRestricciones();
-		}).collect(Collectors.toList());
+        List<Dispositivo> dispositivosConRestricciones = filtrarDispositivosConRestricciones(dispositivos);
 
-		// Funcion economica-objetivo
-		double[] arrayObjetivo = (double[]) ArrayUtils.toPrimitive(dispositivosConRestricciones.stream().map(disp -> {
-			return 1;
-		}).collect(Collectors.toList()));
+        LinearObjectiveFunction funcionZ = generarFuncionEconomica(dispositivosConRestricciones);
 
-		LinearObjectiveFunction funcionZ = new LinearObjectiveFunction(arrayObjetivo, 0);
+        List<LinearConstraint> restricciones = generarRestricciones(dispositivosConRestricciones);
 
-		// RESTRICCIONES
-		List<LinearConstraint> restricciones = new ArrayList<LinearConstraint>();
+        PointValuePair solucion = solucionarSimplex(funcionZ, restricciones);
 
-		// Restriccion mensual
-		double[] coeficientesRestriccion = arrayDeCoeficientes(dispositivosConRestricciones);
-		LinearConstraint restriccionMensual = new LinearConstraint(coeficientesRestriccion, Relationship.LEQ,
-				limiteMensual);
-		restricciones.add(restriccionMensual);
+        return generarSolucion(solucion, dispositivosConRestricciones);
+    }
 
-		// Restricciones por dispositivo
-		dispositivosConRestricciones.forEach(dispositivo -> {
-			LinearConstraint restriccionSuperior = generarRestricciones(dispositivosConRestricciones, dispositivo,
-					Relationship.LEQ);
-			LinearConstraint restriccionInferior = generarRestricciones(dispositivosConRestricciones, dispositivo,
-					Relationship.GEQ);
-			restricciones.add(restriccionSuperior);
-			restricciones.add(restriccionInferior);
-		});
+    private static List<Optimizacion> generarSolucion(PointValuePair solucion, List<Dispositivo> dispositivos){
+        List<Double> limitesDeRestriccion = listFromDoubleArray(solucion.getPoint());
+        return generarOptimizaciones(dispositivos, limitesDeRestriccion);
+    }
 
-		// Solucionar problema
-		SimplexSolver solver = new SimplexSolver();
-		PointValuePair solucion = solver.optimize(new MaxIter(100) // Numero maximo de iteraciones
-				, funcionZ // Funcion objetivo
-				, new LinearConstraintSet(restricciones) // Restricciones
-				, GoalType.MAXIMIZE // Objetivo (Maximizar)
-				, new NonNegativeConstraint(true)); // Restriccion adicional ( solo positivos )
+    private static PointValuePair solucionarSimplex(LinearObjectiveFunction funcionZ, List<LinearConstraint> restricciones) {
+        SimplexSolver solver = new SimplexSolver();
+        return solver.optimize(new MaxIter(100) // Numero maximo de iteraciones
+                    , funcionZ // Funcion objetivo
+                    , new LinearConstraintSet(restricciones) // Restricciones
+                    , GoalType.MAXIMIZE // Objetivo (Maximizar)
+                    , new NonNegativeConstraint(true));
+    }
 
-		// Genear solucion
-		List<Double> limitesDeRestriccion = listFromDoubleArray(solucion.getPoint());
-		return generarOptimizaciones(dispositivosConRestricciones, limitesDeRestriccion);
-	}
+    private static LinearConstraint generarResticcionMensual(List<Dispositivo> dispositivosConRestricciones) {
+        Double limiteMensual = ConfiguracionApp.limiteMensualDeConsumo;
+        double[] coeficientesRestriccion = arrayDeCoeficientes(dispositivosConRestricciones);
+        return new LinearConstraint(coeficientesRestriccion, Relationship.LEQ,
+                limiteMensual);
+    }
 
-	private static List<Double> listFromDoubleArray(double[] array) {
-		return DoubleStream.of(array).boxed().collect(Collectors.toList());
-	}
+    private static List<Dispositivo> filtrarDispositivosConRestricciones(List<Dispositivo> dispositivos) {
+        return dispositivos.stream().filter(Dispositivo::tieneRestricciones).collect(Collectors.toList());
+    }
 
-	private static LinearConstraint generarRestricciones(List<Dispositivo> dispositivos, Dispositivo dispositivo,
-			Relationship relacion) {
-		double[] arrayRestriccion = (double[]) ArrayUtils.toPrimitive(dispositivos.stream().map(disp -> {
-			if (disp == dispositivo) {
-				return 1d;
-			} else {
-				return 0d;
-			}
-		}).collect(Collectors.toList()));
+    private static LinearObjectiveFunction generarFuncionEconomica(List<Dispositivo> dispositivosConRestricciones) {
+        double[] arrayObjetivo = (double[]) ArrayUtils.toPrimitive(dispositivosConRestricciones.stream().map(disp -> 1).collect(Collectors.toList()));
+        return new LinearObjectiveFunction(arrayObjetivo, 0);
+    }
 
-		double limite;
-		if (relacion == Relationship.LEQ) {
-			limite = dispositivo.restricciones().getCotaSuperior().doubleValue();
-		} else if (relacion == Relationship.GEQ) {
-			limite = dispositivo.restricciones().getCotaInferior().doubleValue();
-		} else {
-			throw new RuntimeException("Relacion no admitida");
-		}
+    private static List<Double> listFromDoubleArray(double[] array) {
+        return DoubleStream.of(array).boxed().collect(Collectors.toList());
+    }
 
-		LinearConstraint restriccion = new LinearConstraint(arrayRestriccion, relacion, limite);
-		return restriccion;
-	}
+    private static LinearConstraint generarRestriccion(List<Dispositivo> dispositivos, Dispositivo dispositivo,
+                                                       Relationship relacion) {
+        double[] arrayRestriccion = (double[]) ArrayUtils.toPrimitive(dispositivos.stream().map(disp -> {
+            if (disp == dispositivo) {
+                return 1d;
+            } else {
+                return 0d;
+            }
+        }).collect(Collectors.toList()));
 
-	private static double[] arrayDeCoeficientes(List<Dispositivo> dispositivos) {
-		return (double[]) ArrayUtils.toPrimitive(
-				dispositivos.stream().map(dispositivo -> dispositivo.consumoPorHora()).collect(Collectors.toList()));
-	}
+        double limite;
+        if (relacion == Relationship.LEQ) {
+            limite = dispositivo.restricciones().getCotaSuperior();
+        } else if (relacion == Relationship.GEQ) {
+            limite = dispositivo.restricciones().getCotaInferior();
+        } else {
+            throw new RuntimeException("Relacion no admitida");
+        }
 
-	private static List<Optimizacion> generarOptimizaciones(List<Dispositivo> dispositivos, List<Double> limites) {
-		return Utils.zip(dispositivos.stream(), limites.stream(), (dispositivo, limite) -> {
-			return new Optimizacion(dispositivo, limite);
-		}).collect(Collectors.toList());
-	}
+        return new LinearConstraint(arrayRestriccion, relacion, limite);
+    }
+
+    private static double[] arrayDeCoeficientes(List<Dispositivo> dispositivos) {
+        return (double[]) ArrayUtils.toPrimitive(
+                dispositivos.stream().map(Dispositivo::consumoPorHora).collect(Collectors.toList()));
+    }
+
+    private static List<Optimizacion> generarOptimizaciones(List<Dispositivo> dispositivos, List<Double> limites) {
+        return Utils.zip(dispositivos.stream(), limites.stream(), (dispositivo, limite) -> {
+            return new Optimizacion(dispositivo, limite);
+        }).collect(Collectors.toList());
+    }
+
+    private static List<LinearConstraint> generarRestricciones(List<Dispositivo> dispositivos){
+        // RESTRICCIONES
+        List<LinearConstraint> restricciones = new ArrayList<>();
+
+        // Restriccion mensual
+        LinearConstraint restriccionMensual = generarResticcionMensual(dispositivos);
+        restricciones.add(restriccionMensual);
+
+        // Restricciones por dispositivo
+        dispositivos.forEach(dispositivo -> {
+            LinearConstraint restriccionSuperior = generarRestriccion(dispositivos, dispositivo,
+                    Relationship.LEQ);
+            LinearConstraint restriccionInferior = generarRestriccion(dispositivos, dispositivo,
+                    Relationship.GEQ);
+            restricciones.add(restriccionSuperior);
+            restricciones.add(restriccionInferior);
+        });
+        return restricciones;
+    }
 
 }
