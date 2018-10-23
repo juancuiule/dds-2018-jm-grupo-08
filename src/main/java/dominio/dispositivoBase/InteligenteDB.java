249e4d9035d01a09100ee6c4acdfb5787ed651eb
maribelAldana <maribel.aldana.sandoval@gmail.com>
2018-10-23 01:21:00 -0300

    mapeo nuevo

--- src/main/java/dominio/dispositivoBase/InteligenteDB.java
+++ src/main/java/dominio/dispositivoBase/InteligenteDB.java
@@ -1,6 +1,8 @@
 package dominio.dispositivoBase;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 import dominio.dispositivo.ComportamientoInteligente;
 import dominio.dispositivo.Dispositivo;
@@ -10,6 +12,7 @@ import dominio.dispositivo.Rango;
 @Entity
 public class InteligenteDB extends DispositivoBase{
 	
+	@OneToOne(cascade = CascadeType.PERSIST)
 	private DispositivoFisico dispositivoFisico;
 	
 	public InteligenteDB(String nombre, Double cotaSuperior, Double cotaInferior, Double consumoPorHora, DispositivoFisico dispositivoFisico) {
