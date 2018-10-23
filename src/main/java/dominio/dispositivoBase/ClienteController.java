6e5aa9ec6c1fd6d62fee3b617f2475f384a718a1
maribelAldana <maribel.aldana.sandoval@gmail.com>
2018-10-23 01:23:57 -0300

    dispositivo factory desde la BD

--- src/main/java/dominio/dispositivoBase/ClienteController.java
+++ src/main/java/dominio/dispositivoBase/ClienteController.java
@@ -2,16 +2,14 @@ package dominio.dispositivoBase;
 
 import dominio.Cliente;
 import dominio.dispositivo.Dispositivo;
+import dominio.dispositivo.DispositivoFactory;
 
 import java.util.Optional;
 
 public class ClienteController {
 
-	public void agregarUnDispositivoAunCliente(Cliente cliente, String nombre) {
-		Optional<DispositivoBase> dispositivo = RepositorioDispositivoBase.getInstance().buscarDispositivo(nombre);
-		
-		Dispositivo dispositivoParaElCliente = dispositivo.get().devolverDisositivo();
-		
-		cliente.agregarDispositivo(dispositivoParaElCliente);
+	public void agregarUnDispositivoAunCliente(Cliente cliente, String nombre) {		
+		DispositivoFactory dispositivoParaElCliente = new DispositivoFactory();
+		cliente.agregarDispositivo(dispositivoParaElCliente.dispositivoFactory(nombre));
 	}
 }
