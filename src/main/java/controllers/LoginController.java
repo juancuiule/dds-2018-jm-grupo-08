63545a34c6c33abeae95a73dcf2ec32f153b7b89
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-10-24 09:54:13 -0300

    pantalla login con partials de nav y head

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -11,26 +11,25 @@ import java.util.Map;
 import java.util.Optional;
 
 public class LoginController {
-    private static Authenticator auth = new Authenticator();
+	private static Authenticator auth = new Authenticator();
 
-    public static ModelAndView respond(Request req, Response res) {
-        Map<String, Object> viewModel = new HashMap<>();
-        return new ModelAndView(viewModel,"login.hbs");
-    }
+	public static ModelAndView respond(Request req, Response res) {
+		Map<String, Object> viewModel = new HashMap<>();
+		return new ModelAndView(viewModel, "login.hbs");
+	}
 
-    public static String react(Request req, Response res) {
-        String username = req.queryParams("username");
-        String password = req.queryParams("password");
-        System.out.println(req.body());
-        Optional<Usuario> usuario = auth.authenticateUser(username, password);
+	public static String react(Request req, Response res) {
+		String username = req.queryParams("username");
+		String password = req.queryParams("password");
+		Optional<Usuario> usuario = auth.authenticateUser(username, password);
 
-        if(usuario.isPresent()){
-            req.session().attribute("auth",true);
-            req.session().attribute("user",usuario.get());
-            res.redirect("/roleSelection");
-        }else{
-            return "Usuario y/o contraseña invalidos";
-        }
-        return null;
-    }
+		if (usuario.isPresent()) {
+			req.session().attribute("auth", true);
+			req.session().attribute("user", usuario.get());
+			res.redirect("/roleSelection");
+		} else {
+			return "Usuario y/o contraseña invalidos";
+		}
+		return null;
+	}
 }
--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -10,44 +10,43 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.Optional;
 
 public class Router {
-    public static void init() {
-        HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+	public static void init() {
+		HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+		Spark.staticFiles.location("/public");
 
-        Spark.get("/", LandingController::respond);
-        Spark.get("/login", LoginController::respond,engine);
-        Spark.post("/login", LoginController::react);
-        Spark.get("/admin", AdminController::respond);
-        Spark.get("/admin/reporte", AdminReporteController::respond);
-        Spark.get("/roleSelection", RoleSelectionController::respond);
+		Spark.get("/", LandingController::respond);
+		Spark.get("/login", LoginController::respond, engine);
+		Spark.post("/login", LoginController::react);
+		Spark.get("/admin", AdminController::respond);
+		Spark.get("/admin/reporte", AdminReporteController::respond);
+		Spark.get("/roleSelection", RoleSelectionController::respond);
 
+		Spark.before("/user*", (req, res) -> {
+			haltIfNotAuthenticated(req);
+			redirectRootIfTrue(res, !obtenerUsuario(req).esCliente());
+		});
+		Spark.before("/admin*", (req, res) -> {
+			haltIfNotAuthenticated(req);
+			redirectRootIfTrue(res, !obtenerUsuario(req).esAdmin());
+		});
+	}
 
-        Spark.before("/user*", (req, res) -> {
-            haltIfNotAuthenticated(req);
-            redirectRootIfTrue(res, !obtenerUsuario(req).esCliente());
-        });
-        Spark.before("/admin*", (req, res) -> {
-            haltIfNotAuthenticated(req);
-            redirectRootIfTrue(res, !obtenerUsuario(req).esAdmin());
-        });
-    }
+	private static void haltIfNotAuthenticated(Request req) {
+		if (!isAuthenticated(req)) {
+			Spark.halt(401, "Acceso restingido");
+		}
+	}
 
-    private static void haltIfNotAuthenticated(Request req) {
-        if (!isAuthenticated(req)) {
-            Spark.halt(401, "Acceso restingido");
-        }
-    }
+	private static Boolean isAuthenticated(Request req) {
+		return Optional.ofNullable((Boolean) req.session().attribute("auth")).orElse(false);
+	}
 
-    private static Boolean isAuthenticated(Request req) {
-        return Optional.ofNullable
-                ((Boolean) req.session().attribute("auth"))
-                .orElse(false);
-    }
+	private static void redirectRootIfTrue(Response res, Boolean condition) {
+		if (condition)
+			res.redirect("/");
+	}
 
-    private static void redirectRootIfTrue(Response res, Boolean condition) {
-        if (condition) res.redirect("/");
-    }
-
-    private static Usuario obtenerUsuario(Request req) {
-        return req.session().attribute("user");
-    }
+	private static Usuario obtenerUsuario(Request req) {
+		return req.session().attribute("user");
+	}
 }
