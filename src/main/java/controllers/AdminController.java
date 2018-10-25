d4c4eb23790bdf8d37a4391b02aee631979c9e74
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-10-24 21:55:27 -0300

    parse data desde controller de admin al dar de alta dispositivo

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -14,14 +17,24 @@ public class AdminController {
 		// mandar la data del reporte al mostrar
 		return new ModelAndView(null, "admin-dashboard.hbs");
 	}
-	
+
 	public static ModelAndView altaDispositivo(Request req, Response res) {
 		// mandar las interfaces disponibles
 		return new ModelAndView(null, "alta-de-dispositivo.hbs");
 	}
 
 	public static String darDeAlta(Request req, Response res) {
-		System.out.println(req.body());
+		JsonParser jsonParser = new JsonParser();
+		JsonObject jsonObject = (JsonObject) jsonParser.parse(req.body());
+
+		String nombre = jsonObject.get("nombre").getAsString();
+		Boolean esInteligente = jsonObject.get("inteligente").getAsBoolean();
+		Double consumoPorHora = jsonObject.get("consumoPorHora").getAsDouble();
+		String interfaz;
+		if (esInteligente) {
+			interfaz = jsonObject.get("interfaz").getAsString();
+		}
+
 		// todo: dar de alta el dispositivo
 		return "{ \"message\": \"se creo un nuevo dispositivo\" }";
 	}
