3e6d60a2c96f04b867b24415e4726aa5e8a39fcb
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-10-25 03:06:23 -0300

    Frontend de alta de dispositivos

--- src/main/java/dominio/ConfiguracionApp.java
+++ src/main/java/dominio/ConfiguracionApp.java
@@ -1,11 +1,13 @@
 package dominio;
 
+import utils.IdentificadorClase;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class ConfiguracionApp {
 
 	public static Double limiteMensualDeConsumo = 612d;
-	public static List<String> interfacesFisicas = Arrays.asList("DispositivoFisicoGenerico");
+	public static List<IdentificadorClase> interfacesFisicas = Arrays.asList(new IdentificadorClase("DispositivoFisicoGenerico"));
 
 }
